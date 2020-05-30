399ba3d87fd2e4707a9b3e60e507b8fba31ed0f9
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2020-05-30 18:30:36 +0530

    render html elements

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import { createTextElement, render } from './lib/react';
  * React text element
  */
 const textEl = createTextElement('Hello World');
+const htmlEl = {
+  type: 'h1',
+  props: {
+    children: [textEl],
+  },
+};
 
 const container = document.getElementById('⚛️');
-render(textEl, container);
+render(htmlEl, container);
--- src/lib/react/index.js
+++ src/lib/react/index.js
@@ -1,6 +1,24 @@
 export const render = (element, container) => {
-  const dom = document.createTextNode('');
-  dom.nodeValue = element.props.nodeValue;
+  const dom =
+    element.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(element.type);
+
+  const isProperty = (key) => key !== 'children';
+
+  Object.keys(element.props)
+    .filter(isProperty) // filter children prop
+    .forEach((name) => {
+      /**
+       * Add props like nodeValue
+       */
+      dom[name] = element.props[name];
+    });
+
+  if (element.props.children) {
+    element.props.children.forEach((child) => render(child, dom)); // recursively render children
+  }
+
   container.appendChild(dom);
 };
 
