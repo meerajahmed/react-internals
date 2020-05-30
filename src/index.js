e4379ac2728987ba4f645eae41be7f085c4a6af7
Meeraj Ahmed <meeraj.ahmed@publicissapient.com>
2020-05-30 18:16:00 +0530

    add create text element helper

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import { render } from './lib/react';
+import { createTextElement, render } from './lib/react';
 
 /**
  * React text element
  */
-const textEl = {
-  type: 'TEXT_ELEMENT',
-  props: {
-    nodeValue: 'Hello World',
-  },
-};
+const textEl = createTextElement('Hello World');
 
 const container = document.getElementById('⚛️');
 render(textEl, container);
--- src/lib/react/index.js
+++ src/lib/react/index.js
@@ -1,6 +1,14 @@
-// eslint-disable-next-line import/prefer-default-export
 export const render = (element, container) => {
   const dom = document.createTextNode('');
   dom.nodeValue = element.props.nodeValue;
   container.appendChild(dom);
+};
+
+export const createTextElement = (text) => {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+    },
+  };
 };
